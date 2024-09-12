6073d1ef70fb21be5087ef7169eb9ae0b7f5641e
Pavel Shitikov (mac) <pashicop@gmail.com>
2024-09-12 15:32:20 +0300

    find only map

--- main.py
+++ main.py
@@ -3,19 +3,49 @@ from pprint import pprint
 import pandas as pd
 import openpyxl
 import re
+import ast
 
 
-def parse(text: str):
+def format_string(string, p):
+    f_string = string.replace(p, '')
+    key_pattern = r'(\w+):'
+    p_regex = re.compile(key_pattern)
+    keys = re.findall(p_regex, f_string)
+    print(keys)
+    f_string = re.sub(p_regex, r'"\1":', f_string)
+    print(f_string)
+    return f_string
+
+
+def parse(text: str, lang:'en'):
     # translation_string = text[text.find('={map:{'):text.rfind('}')]
-    p = re.compile(r'{map:{.*?}')
-    translation_string = p.search(text)
-    if translation_string:
-        print(f'Найдено({len(translation_string.group())}): {translation_string.group()}')
-    else:
-        print(f'Не найдено')
-    dict_string = translation_string.group().replace('{map:{', '')
-    print(len(dict_string), dict_string)
-
+    if lang == 'en':
+        pattern = '{map:'
+        reg_exp = r'{map:{.*?}'
+        pattern_re = re.compile(reg_exp)
+        translation_string = pattern_re.search(text)
+        if not translation_string:
+            print(f'Не найдено')
+        else:
+            print(f'Найдено({len(translation_string.group())}): {translation_string.group()}')
+            formatted_string = format_string(translation_string.group(), pattern)
+            translation_dict = eval(formatted_string)
+            print(type(translation_dict), len(translation_dict), translation_dict)
+            return translation_dict
+    elif lang == 'ru':
+        pattern = '{map:'
+        # reg_exp = r'{map:{.*:"[А-я]*?}'
+        reg_exp = r'{map:{.*?:"[А-я]+?".*?}'
+        pattern_re = re.compile(reg_exp)
+        translation_string = pattern_re.search(text)
+        if not translation_string:
+            print(f'Не найдено')
+        else:
+            print(f'Найдено({len(translation_string.group())}): {translation_string.group()}')
+            formatted_string = format_string(translation_string.group(), pattern)
+            translation_dict = eval(formatted_string)
+            print(type(translation_dict), len(translation_dict), translation_dict)
+            return translation_dict
 
 def read_from_file(file="data/app.fc4d0722.js"):
     with open(file) as f:
@@ -24,31 +54,13 @@ def read_from_file(file="data/app.fc4d0722.js"):
 
 
 def main():
-    # data = pd.read_excel('data/olta.xlsx')
-    # # pprint(data["Телефон"])
-    # chars_to_remove = [' ', '-', '‑', '(', ')', '+']
-    # tel_series = pd.Series
-    # tel_list = list()
-    # for i, tel in enumerate(data["Телефон"]):
-    #     tel_formatted = (str(tel).strip() + '.')[:-1]
-    #     for char in chars_to_remove:
-    #         tel_formatted = tel_formatted.replace(char, "")
-    #     if len(tel_formatted) == 10:
-    #         tel_formatted = '8' + tel_formatted
-    #     if len(tel_formatted) == 11 and tel_formatted[1] == '9':
-    #         tel_list.append(tel_formatted)
-    #     else:
-    #         tel_list.append('')
-    #     print(i, tel, tel_formatted, type(tel_formatted))
-    # tel_series = tel_list
-    # data["tel"] = tel_series
-    # print(data['tel'].head())
-    # data.to_excel("olta_formatted.xlsx", sheet_name="Лист1")
-    # # print(f'\nКоличество номеров: {data["Телефон"].size}')
     src_text = read_from_file()
     if src_text:
-        parse(src_text)
+        # dict_from_str = parse(src_text, 'ru')
+        dict_from_str = parse(src_text, 'en')
 
+        data = pd.DataFrame.from_dict(dict_from_str, orient='index')
+        data.to_excel("Шторм.xlsx", sheet_name="Лист1")
 
 
 if __name__ == "__main__":
