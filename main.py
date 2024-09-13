46dc5a5da16271a7f58e94b0fd404b9327c80a35
Pavel Shitikov (mac) <pashicop@gmail.com>
2024-09-13 11:11:00 +0300

    final for read js file, parse and write to xlsx

--- main.py
+++ main.py
@@ -8,26 +8,19 @@ import ast
 
 
 def format_string(string, p):
-    # f_string = string.replace(p, '')
     f_string = string[len(p):]
     if p == 'msg:' or p == 'set:':
-        print(f_string)
+        # print(f_string)
         del_part = r'\w+?(: )'
         del_regex = re.compile(del_part)
         f_string = re.sub(del_regex, r'- ', f_string)
-        # f_string = f_string.replace('unit: meter', "unit - meter")
-        # f_string = f_string.replace('单位:', "单位-")
-        # f_string = f_string.replace('единица измерения: метр', "единица измерения - метр")
-        # f_string = f_string.replace('الوحدة:', "الوحدة-")
-        # f_string = f_string.replace('unité:', "unité-")
-        # f_string = f_string.replace('unidad:', "unidad-")
-        print(f_string)
+        # print(f_string)
     key_pattern = r'(\w+):'
     p_regex = re.compile(key_pattern)
     keys = re.findall(p_regex, f_string)
-    print(len(keys), keys)
+    # print(len(keys), keys)
     f_string = re.sub(p_regex, r'"\1":', f_string)
-    print(f_string)
+    # print(f_string)
     return f_string
 
 
@@ -35,20 +28,17 @@ def has_cyrillic(text):
     return bool(re.search('[а-яА-Я]', text))
 
 
-def has_latin(text):
-    result = False
-    for char in text:
-        if char.lower() in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','!','?']:
-            result = True
-        else:
-            return False
-    return result
+# def has_latin(text):
+#     result = False
+#     for char in text:
+#         if char.lower() in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','!','?']:
+#             result = True
+#         else:
+#             return False
+#     return result
 
 
 def parse(text: str):
-    # translation_string = text[text.find('={map:{'):text.rfind('}')]
-    # pattern = 'window:'
-    # reg_exp = r'window:{.*?}'
     patterns = {'map': ["nameCannotNull",'Please enter the complete name'],
                 'window': ["savedSuccessfully", "Saved successfully"],
                 'msg': ["ring_one", "ring one"],
@@ -70,19 +60,16 @@ def parse(text: str):
             print(f'Найдено({len(translation_string)}): {translation_string}')
             for t_str in translation_string:
                 formatted_string = format_string(t_str, pattern)
-                # translation_dict = eval(formatted_string)
                 translation_dict = ast.literal_eval(formatted_string)
                 if translation_dict[value[0]] == value[1]:
                     dict_en = deepcopy(translation_dict)
-                    # dict_en = {"a": 12, "b": "bbb", "c": "ccc", "d": "ddd",}
                 elif has_cyrillic(translation_dict[value[0]]):
                     dict_ru = deepcopy(translation_dict)
-                    # dict_ru = {"a": 13, "b": "ЫВАВАЫВВ", "d": "ываыва", "e": "ЫВАВЫАААА"}
             if dict_ru:
                 if dict_en:
                     dd = defaultdict(list)
                     dd[pattern] = ['', '']
-                    list_dict_en_keys = list(dict_en.keys())
+                    # list_dict_en_keys = list(dict_en.keys())
                     list_dict_ru_keys = list(dict_ru.keys())
                     for key in list(dict_en.keys()):
                         dd[key].append(dict_en[key])
@@ -114,11 +101,7 @@ def read_from_file(file="data/app.fc4d0722.js"):
 def main():
     src_text = read_from_file()
     if src_text:
-        # dict_from_str = parse(src_text, 'ru')
         parse(src_text)
-
-        # data = pd.DataFrame.from_dict(dict_from_str, orient='index')
-        # data.to_excel("Шторм.xlsx", sheet_name="Лист1")
 
 
 if __name__ == "__main__":
